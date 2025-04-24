80bbe43c4a7473876d3a4e5d6f95469480976329
kiwan <a.kiwan@ejadtech.sa>
2025-04-25 01:27:36 +0300

    fix submit

--- lib/services/submisson_service.dart
+++ lib/services/submisson_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class SubmissionService {
   static Future<void> submitAnswers(Map<String, dynamic> answers) async {
     final url = Uri.parse(
-        'https://script.google.com/macros/s/AKfycbzBr0rOsvTtm1Qn737p2qL1Ze1CzTo0Wc6ZYEbYKXJSpahRGS7dNU-Il2bm44pL268BxA/exec');
+        'https://script.google.com/macros/s/AKfycbzLE0Smf8Q7uC4I-bRD3OMyKX-pcFhnrfZpwV6idbtzGx0E2XilP3csUHONlJnVApT0rQ/exec');
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
